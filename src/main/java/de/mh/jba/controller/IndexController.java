4a5910293c5e801173a81a8ac644443b4f8e3a7d
Mas Haber <mh.bato@googlemail.com>
2014-05-27 15:14:41 +0200

    Spring web app tutorial 3: Spring Web MVC hello world

---
+++ src/main/java/de/mh/jba/controller/IndexController.java
@@ -0,0 +1,16 @@
+package de.mh.jba.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "/WEB-INF/jsp/index.jsp";
+		
+		
+	}
+	
+}
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,6 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-hallo welt
+
+hello from spring mvc
+
 </body>
 </html>
