eba39765f2dbfa3ba86da44f229ef05189799f2f
Mas Haber <mh.bato@googlemail.com>
2014-06-11 15:14:17 +0200

    Spring web app tutorial 47: Scheduling

--- src/main/java/de/mh/jba/service/BlogService.java
+++ src/main/java/de/mh/jba/service/BlogService.java
@@ -3,6 +3,7 @@ package de.mh.jba.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -41,24 +42,29 @@ public class BlogService {
 	public void saveItems(Blog blog) {
 		try {
 			List<Item> items = rssService.getItems(blog.getUrl());
-			for (Item item : items) {
-				
+			for (Item item : items) {			
 				Item savedItem = itemRepository.findByBlogAndLink(blog, item.getLink());
 				if (savedItem == null) {
 					item.setBlog(blog);
 					itemRepository.save(item);
-				}
-				
-			}
-			
+				}				
+			}			
 		} catch (RssException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
 
 
+	// 1 hour = 60 seconds X 60 minutes X 1000
+	@Scheduled(fixedDelay=3600000)
+	public void reloadBlogs() {
+		List<Blog> blogs = blogRepository.findAll();
+		for (Blog blog : blogs) {
+			saveItems(blog);
+		}
+	}
+	
 	public void delete(int id) {
 		blogRepository.delete(id);
 	}
